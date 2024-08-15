cd33e0b91a05e982d5b11e62ca54d2d63cbd191d
Nino Gasparus <ngasparus@gmail.com>
2024-08-15 18:39:22 +0200

    Removed useless code

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-
-include "components/conn.php";
-if($debug == true){
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head> 
